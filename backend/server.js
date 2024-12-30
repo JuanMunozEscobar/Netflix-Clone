f8fb3d206414aca77e50f7c129348e6626dddd0c
Tony <munoztony329@gmail.com>
2024-12-30 10:49:05 -0600

    created routes for signup login & logout

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 import express from 'express';
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 
-app.get("/", (req, res) =>{
-    res.send("Sever is Ready");
-})
+app.use("/api/v1/auth", authRoutes)
+
 
 app.listen(5000, () => {
     console.log('Server started at http://localhost:5000');
