a9f6c9dfc8b0231d574c7104777fe1701437141c
Tony <munoztony329@gmail.com>
2025-01-03 12:48:38 -0600

    created & implemented routes for TV all of which are simliar to movie routes

---
+++ backend/controller/tv.controller.js
@@ -0,0 +1,65 @@
+import { fetchFromTMDB } from "../services/tmdb.service.js";
+
+export async function getTrendingTV(req, res){
+    try {
+        const data = await fetchFromTMDB('https://api.themoviedb.org/3/trending/tv/day?language=en-US');
+        const randomTV = data.results[Math.floor(Math.random() * data.results?.length)];
+
+        res.status(200).json({success: true, content: randomTV})
+    } catch (error) {
+        console.log("Error: ", error.message);
+        res.status(500).json({success: false, message: "Internal Sever Error"});
+    }
+}
+export async function getTvTrailers(req, res){
+    const {id} = req.params;
+    try {
+        const data = await fetchFromTMDB(`https://api.themoviedb.org/3/tv/${id}/videos?language=en-US`);
+        res.json({success: true, trailers: data.results});
+
+    } catch (error) {
+        if(error.message.includes("404")){
+            console.log("Error: " ,error.message);
+            return res.status(404).send(null);
+        }
+        console.log("Error: ",error.message);
+        res.status(500).json({success: false, message: "Internal Sever Error"});
+
+        
+    }
+}
+
+export async function getTvDetails(req,res){
+    const {id} = req.params;
+    try {
+        const data = await fetchFromTMDB(`https://api.themoviedb.org/3/tv/${id}?language=en-US`);
+        res.status(200).json({success: true, content: data});
+    } catch (error) {
+        console.log("Error: ", error.message);
+        res.status(500).json({success: false, message: "Internal Sever Error"});
+    }
+}
+
+export async function getSimilarTv(req,res){
+    const {id} = req.params;
+    try {
+        const data = await fetchFromTMDB(`https://api.themoviedb.org/3/tv/${id}/similar?language=en-US&page=1`);
+        res.status(200).json({success: true, similar: data});
+    } catch (error) {
+        console.log("Error: ", error.message);
+        res.status(500).json({success: false, message: "Internal Sever Error"});
+        
+    }
+}
+
+export async function getTvByCatagory(req, res){
+    const { catagory } = req.params;
+    try {
+        const data = await fetchFromTMDB(`https://api.themoviedb.org/3/tv/${catagory}?language=en-US&page=1`);
+        res.status(200).json({success: true, content: data.results})
+
+    } catch (error) {
+        console.log("Error: ", error.message);
+        res.status(500).json({success: false, message: "Internal Sever Error"});
+    }
+}
---
+++ backend/routes/tv.route.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { getTrendingTV, getTvDetails, getTvTrailers , getSimilarTv, getTvByCatagory} from "../controller/tv.controller.js";
+
+const router = express.Router();
+
+router.get("/trending", getTrendingTV);
+router.get("/:id/trailers", getTvTrailers);
+router.get("/:id/details", getTvDetails);
+router.get("/:id/similar", getSimilarTv);
+router.get("/:catagory", getTvByCatagory);
+
+export default router;
