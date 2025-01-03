6711598aea0528c019225eb7ae299c641955233c
Tony <munoztony329@gmail.com>
2025-01-02 21:16:00 -0600

    created & implemented get movie trailer by id function

--- backend/controller/movie.controller.js
+++ backend/controller/movie.controller.js
@@ -13,3 +13,21 @@ export async function getTrendingMovie (req,res){
         res.status(500).json({success:false, message: "Internal Sever Error"});
     }
 }
+
+export async function getMovieTrailers(req, res){
+    const {id} = req.params;
+    try {
+        const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`);
+        res.json({success: true, trailers: data.results});
+
+    } catch (error) {
+        if(error.message.includes("404")){
+            console.log("Error: " ,error.message);
+            return res.status(404).send(null);
+        }
+        console.log("Error: ",error.message);
+        res.status(500).json({success: false, message: "Internal Sever Error"});
+
+        
+    }
+}
--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { getTrendingMovie } from '../controller/movie.controller.js';
+import { getMovieTrailers } from '../controller/movie.controller.js';
 
 
 const router = express.Router();
 
 router.get("/trending", getTrendingMovie);
+router.get("/:id/trailers", getMovieTrailers)
 
 export default router;
