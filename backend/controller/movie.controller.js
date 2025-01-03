08097946f58c73359c2b9f2186dc2d07aad44798
Tony <munoztony329@gmail.com>
2025-01-03 11:41:58 -0600

    created & implemented functions for getting movies details &  for similar moives

--- backend/controller/movie.controller.js
+++ backend/controller/movie.controller.js
@@ -31,3 +31,26 @@ export async function getMovieTrailers(req, res){
         
     }
 }
+
+export async function getMovieDetails(req,res){
+    const {id} = req.params;
+    try {
+        const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}?language=en-US`);
+        res.status(200).json({success: true, content: data});
+    } catch (error) {
+        console.log("Error: ", error.message);
+        res.status(500).json({success: false, message: "Internal Sever Error"});
+    }
+}
+
+export async function getSimilarMovies(req,res){
+    const {id} = req.params;
+    try {
+        const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}/similar?language=en-US&page=1`);
+        res.status(200).json({success: true, similar: data});
+    } catch (error) {
+        console.log("Error: ", error.message);
+        res.status(500).json({success: false, message: "Internal Sever Error"});
+        
+    }
+}
--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { getTrendingMovie } from '../controller/movie.controller.js';
-import { getMovieTrailers } from '../controller/movie.controller.js';
+import { getMovieDetails, getTrendingMovie , getMovieTrailers, getSimilarMovies } from '../controller/movie.controller.js';
+
 
 
 const router = express.Router();
 
 router.get("/trending", getTrendingMovie);
-router.get("/:id/trailers", getMovieTrailers)
+router.get("/:id/trailers", getMovieTrailers);
+router.get("/:id/details", getMovieDetails);
+router.get("/:id/similar", getSimilarMovies);
 
 export default router;
