c87eaad0977df383508f6d29d29ce56da6436858
Tony <munoztony329@gmail.com>
2025-01-04 12:47:10 -0600

    created search routes & implemented search person function

---
+++ backend/controller/serach.controller.js
@@ -0,0 +1,57 @@
+import { User } from "../model/user.model.js";
+import { fetchFromTMDB } from "../services/tmdb.service";
+
+export async function searchPerson(req,res){
+    const { query } = req.params
+    try {
+        const response = await fetchFromTMDB(`https://api.themoviedb.org/3/search/person?query=${query}&include_adult=false&language=en-US&page=1`);
+        
+        if(response.results.length === 0){
+            return res.status(404).send(null);
+        }
+
+        await User.findByIdAndUpdate(req.user._id, {
+            $push:{
+                searchHistory: {
+                    id: response.results[0]._id,
+                    image: response.results[0].profile_path,
+                    title: response.results[0].name,
+                    searchType: "person",
+                    createdAt: new Date(),
+                }
+            }
+        });
+        res.status(200).json({success: true, content: response.results});
+
+    } catch (error) {
+        console.log("Error in search person controller: ", error.message)
+        res.status(500).json({success:false, message: "Internal Sever Error"});
+    }
+}
+export async function searchMovie(req,res){
+    const { query } = req.params
+
+    try {
+        const response = await fetchFromTMDB(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`);
+        
+        if(response.results.length === 0){
+            return res.status(404).send(null);
+        }
+        res.json({success: true, content: response.results});
+
+    } catch (error) {
+        console.log("Error: ", error.message)
+    }
+}
+export async function searchTv(req,res){
+    const { query } = req.params
+
+    try {
+        const response = await fetchFromTMDB(`https://api.themoviedb.org/3/search/tv?query=${query}&include_adult=false&language=en-US&page=1`);
+        
+        res.json({success: true, content: response.results});
+
+    } catch (error) {
+        console.log("Error: ", error.message)
+    }
+}
