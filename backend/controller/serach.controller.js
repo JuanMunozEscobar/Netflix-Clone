e6fe1942e849a02b48ebe2244e9ee5f37126f9c2
Tony <munoztony329@gmail.com>
2025-01-04 12:57:30 -0600

    implemented search movie & search tv functions inside search controller

--- backend/controller/serach.controller.js
+++ backend/controller/serach.controller.js
@@ -28,6 +28,7 @@ export async function searchPerson(req,res){
         res.status(500).json({success:false, message: "Internal Sever Error"});
     }
 }
+
 export async function searchMovie(req,res){
     const { query } = req.params
 
@@ -37,21 +38,51 @@ export async function searchMovie(req,res){
         if(response.results.length === 0){
             return res.status(404).send(null);
         }
-        res.json({success: true, content: response.results});
+        await User.findByIdAndUpdate(req.user._id, {
+            $push:{
+                searchHistory: {
+                    id: response.results[0]._id,
+                    image: response.results[0].poster_path,
+                    title: response.results[0].title,
+                    searchType: "movie",
+                    createdAt: new Date(),
+                }
+            }
+        });
+        res.status(200).json({success: true, content: response.results});
 
     } catch (error) {
-        console.log("Error: ", error.message)
+        console.log("Error in search Movie controller: ", error.message);
+        res.status(500).json({success:false, message: "Internal Sever Error"});
+
     }
 }
+
 export async function searchTv(req,res){
     const { query } = req.params
 
     try {
         const response = await fetchFromTMDB(`https://api.themoviedb.org/3/search/tv?query=${query}&include_adult=false&language=en-US&page=1`);
         
-        res.json({success: true, content: response.results});
+        if(response.results.length === 0){
+            return res.status(404).send(null);
+        }
+        await User.findByIdAndUpdate(req.user._id, {
+            $push:{
+                searchHistory: {
+                    id: response.results[0]._id,
+                    image: response.results[0].profile_path,
+                    title: response.results[0].name,
+                    searchType: "tv",
+                    createdAt: new Date(),
+                }
+            }
+        });
+        res.status(200).json({success: true, content: response.results});
 
     } catch (error) {
-        console.log("Error: ", error.message)
+        console.log("Error in search Tv controller: ", error.message)
+        res.status(500).json({success:false, message: "Internal Sever Error"});
+
     }
 }
