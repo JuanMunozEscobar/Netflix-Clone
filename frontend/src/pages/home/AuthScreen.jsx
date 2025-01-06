0d6de87db02182ce30bd7669a02e444173d8a442
Tony <munoztony329@gmail.com>
2025-01-06 13:54:32 -0600

    created 3rd section

--- frontend/src/pages/home/AuthScreen.jsx
+++ frontend/src/pages/home/AuthScreen.jsx
@@ -63,6 +63,8 @@ const AuthScreen = () => {
         </div>
         {/* 2nd Separator */}
         <div className="h-2 w-full bg-[#232323]" aria-hidden="true"/>
+
+        {/* 2nd Section */}
         <div className="py-10 bg-black text-white">
             <div className="flex max-w-6xl mx-auto items-center justify-center md:flex-row flex-col-reverse px-4 md:px-2">
                 {/*left side*/}
@@ -92,6 +94,34 @@ const AuthScreen = () => {
                         Save your favorites easily and always have something to watch
                     </p>
                 </div>
+            </div>
+        </div>
+        {/* 3rd Separator */}
+        <div className="h-2 w-full bg-[#232323]" aria-hidden="true"/>
+
+        {/* 3rd Section */}
+        <div className="py-10 bg-black text-white">
+            <div className="flex max-w-6xl mx-auto items-center justify-center md:flex-row flex-col px-4 md:px-2">
+            {/*left side*/}
+            <div className="flex-1 text-center md:text-left">
+                <h2 className="text-4xl md:text-5xl font-extrabold mb-4">Watch everwhere</h2>
+                <p className="text-lg md:text-xl">
+                    Stream unlimited movies and Tv shows on your phone, tablet, laptop, and TV.
+                </p>
+            </div>
+            {/*right side*/}
+            <div className="flex-1 relative overflow-hidden">
+                <img src="./device-pile.png" alt="Device image" className="mt-4 z-20 relative"/>
+                <video className="absolute top-2 left-1/2 -translate-x-1/2 h-4/6 z-10
+                max-w-[63%]" 
+                playsInline 
+                autoPlay={true}
+                muted
+                loop
+                >
+                    <source src="/video-devices.m4v" type="video/mp4"/>
+                </video>
+            </div>
             </div>
         </div>
     </div>
