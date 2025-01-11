92905ff70cf51ba8ed55db2a959599121a0b07a4
Tony <munoztony329@gmail.com>
2025-01-10 22:50:03 -0600

    implemented watchPage

--- frontend/src/pages/WatchPage.jsx
+++ frontend/src/pages/WatchPage.jsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import { useContentStore } from "../store/content";
 import axios from "axios";
 import { Navbar } from "../components/Navbar";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import ReactPlayer from "react-player";
+import { ORIGINAL_IMG_BASE_URL, SMALL_IMG_BASE_URL } from "../utils/constant";
+
 
 
 export const WatchPage = () => {
@@ -15,6 +17,16 @@ export const WatchPage = () => {
     const [content, setContent] = useState({});
     const [similarContent, setSimilarContent] = useState([]);
     const { contentType } = useContentStore();
+
+    const sliderRef = useRef(null);
+
+    function formatReleaseDate(date) {
+        return new Date(date).toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+        });
+    }
 
 
     useEffect(() => {
@@ -73,6 +85,18 @@ export const WatchPage = () => {
         }
     }
 
+    const scrollLeft = () => {
+        if (sliderRef.current) {
+            sliderRef.current.scrollBy({ left: -sliderRef.current.offsetWidth, behavior: "smooth" })
+        }
+    }
+    const scrollRight = () => {
+        if (sliderRef.current) {
+            sliderRef.current.scrollBy({ left: sliderRef.current.offsetWidth, behavior: "smooth" })
+        }
+    }
+
+
     return (
         <div className="bg-black min-h-screen text-white">
             <div className="mx-auto container px-4 py-8 h-full">
@@ -120,9 +144,61 @@ export const WatchPage = () => {
                         </h2>
                     )}
                 </div>
+                {/* Movie Details */}
+                <div className="flex flex-col md:flex-row items-center justify-between gap-20 max-w-6xl mx-auto">
+                    <div className="mb-4 md:mb-4">
+                        <h2 className="text-5xl font-bold text-balance">{content?.title || content?.name}</h2>
+                        <p className="mt-2 text-lg">
+                            {formatReleaseDate(content?.release_date || content?.first_air_date)} | {" "}
+                            {content?.adult ? (
+                                <span className="text-red-600">18+</span>) : (
+                                <span className="text-green-600">PG-13</span>
+                            )
+                            }{" "}
+                        </p>
+                        <p className="mt-4 text-lg">{content?.overview}</p>
+                    </div>
+                    <img src={ORIGINAL_IMG_BASE_URL + content.poster_path} alt="Poster Img"
+                        className="max-h-[600px] rounded-md"
+                    />
+                </div>
+                {similarContent.length > 0 && (
+                    <div className="mt-12 max-w-5xl mx-auto">
+                        <h3 className="text-3xl font-bold mb-4">
+                            Similar Movies/Tv Shows
+                        </h3>
+                        <div className="flex overflow-x-scroll scrollbar-hide gap-4 pb-4 group" ref={sliderRef}>
+                            {similarContent.map((content) => (
 
-                
+                                <Link key={content.id} to={`/watch/${content.id}`}
+                                    className="w-52 flex-none"
+                                >
+                                    <img src={SMALL_IMG_BASE_URL + content.poster_path} alt="Poster Path"
+                                        className="w-full h-auto rounded-md"
+                                    />
+                                    <h4 className="mt-2 text-lg font-semibold">
+                                        {content.title || content.name}
+                                    </h4>
+                                </Link>
+
+                            ))}
+                            <ChevronRight
+                                className='absolute top-1/2 -translate-y-1/2 right-2 w-8 h-8
+										opacity-0 group-hover:opacity-100 transition-all duration-300 cursor-pointer
+										bg-red-600 text-white rounded-full'
+                                onClick={scrollRight}
+                            />
+                            <ChevronLeft
+                                className='absolute top-1/2 -translate-y-1/2 left-2 w-8 h-8
+										opacity-0 group-hover:opacity-100 transition-all duration-300 cursor-pointer
+										bg-red-600 text-white rounded-full'
+                                onClick={scrollLeft}
+                            />
+                        </div>
+                    </div>
+                )}
             </div>
-        </div>
+        </div >
     )
 }
+export default WatchPage
