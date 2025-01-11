84a48cbdb5fe32891e7bb12e6bead7da59d4443f
Tony <munoztony329@gmail.com>
2025-01-11 14:27:30 -0600

    created search page & added a search functionality for movies, shows & actors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import { useAuthStore } from "./store/authUser";
 import { useEffect } from "react";
 import { Loader } from "lucide-react";
 import { WatchPage } from "./pages/WatchPage";
-
+import SearchPage from "./pages/SearchPage";
 
 function App() {
   const { user, isCheckingAuth, authCheck } = useAuthStore()
@@ -33,7 +33,9 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='/login' element={!user ? <LoginPage /> : <Navigate to={"/"} />} />
         <Route path='/signup' element={!user ? <SignUpPage /> : <Navigate to={"/"} />} />
-        <Route path='/watch/:id' element={user ? <WatchPage /> : <Navigate to={"/"} />} />
+        <Route path='/watch/:id' element={user ? <WatchPage /> : <Navigate to={"/login"} />} />
+        {/*<Route path='/search' element={user ? <SearchPage /> : <Navigate to={"/login"} />} />*/}
+        <Route path='/search' element={<SearchPage/>} />
       </Routes>
       <Footer />
 
