1205674d3a6193075360a374b70ea01c6960f8ad
Tony <munoztony329@gmail.com>
2025-01-11 21:48:54 -0600

    created & implented search history page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import { useEffect } from "react";
 import { Loader } from "lucide-react";
 import { WatchPage } from "./pages/WatchPage";
 import SearchPage from "./pages/SearchPage";
+import { SearchHistoryPage } from "./pages/SearchHistoryPage";
 
 function App() {
   const { user, isCheckingAuth, authCheck } = useAuthStore()
@@ -34,8 +35,8 @@ function App() {
         <Route path='/login' element={!user ? <LoginPage /> : <Navigate to={"/"} />} />
         <Route path='/signup' element={!user ? <SignUpPage /> : <Navigate to={"/"} />} />
         <Route path='/watch/:id' element={user ? <WatchPage /> : <Navigate to={"/login"} />} />
-        {/*<Route path='/search' element={user ? <SearchPage /> : <Navigate to={"/login"} />} />*/}
-        <Route path='/search' element={<SearchPage/>} />
+        <Route path='/search' element={user ? <SearchPage /> : <Navigate to={"/login"} />} />
+        <Route path='/history' element={user ? <SearchHistoryPage /> : <Navigate to={"/login"} />} />
       </Routes>
       <Footer />
 
